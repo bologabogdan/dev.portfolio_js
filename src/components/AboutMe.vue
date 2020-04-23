5f0fa1bab2707315e40075a11cc4bb8d69c955ac
Bogdan Bologa <35691655+bologabogdan@users.noreply.github.com>
2020-04-23 15:19:25 +0300

    Update

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,22 +1,28 @@
 <template>
   <div>
-    <div class="w-full bg-gray-100 px-8 py-16 lg:py-16">
-      <div class="lg:flex max-w-6xl mx-auto">
-        <div class="lg:w-2/3 self-center">
-          <img
-            alt="Photo of Bogdan Bologa"
-            src="../assets/about-bogdan.png"
-            class="w-2/3 h:-2/3 rounded-full mx-auto block"
-          />
-        </div>
-
-        <div
-          class="bg-white lg:w-2/3 mt-16 lg:mt-0 self-center shadow p-8 rounded-lg lg:ml-16"
-        >
-          <h2 class="font-bold text-xl pb-8">{{ $t("about.1.title") }}</h2>
-          <p>{{ $t("about.1.text") }}</p>
+    <div class="w-full bg-white px-8 py-16 lg:py-16">
+      <div class="max-w-6xl mx-auto">
+        <div class="bg-white lg:w-fill self-center shadow rounded-lg ">
+          <div class="w-full h-full rounded-t-full shadow-inner">
+            <img
+              alt="Photo of Bogdan Bologa"
+              src="../assets/about-bogdan.png"
+              class="w-full rounded-t-lg image"
+            />
+          </div>
+          <div class="p-8 pt-16">
+            <h2 class="font-bold text-2xl pb-8">{{ $t("about.1.title") }}</h2>
+            <p>{{ $t("about.1.text") }}</p>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.image {
+  height: fit-content;
+  object-fit: cover;
+}
+</style>
